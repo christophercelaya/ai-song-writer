3506fa425eadd51064b81c1286256681a8d3b0d8
Chris Celaya <mr.christophercelaya@gmail.com>
2023-01-07 15:47:16 -0700

    working changes

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,40 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
   };
+
   return (
     <div className="root">
       <Head>
         <title>GPT-3 Writer | buildspace</title>
       </Head>
+
       <div className="container">
         <div className="header">
           <div className="header-title">
@@ -27,8 +52,39 @@ const Home = () => {
 
       // Prompt Container
       <div className="prompt-container">
-      <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText} />;
+      <textarea 
+        className="prompt-box" 
+        placeholder="start typing here" 
+        value={userInput} 
+        onChange={onUserChangedText} 
+        />;
       </div>
+
+      // Generate Button
+      <div className="prompt-buttons">
+      <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+     </div>
+
+      // Outputs
+      {apiOutput && (
+      <div className="output">
+        <div className="output-header-container">
+          <div className="output-header">
+            <h3>Output</h3>
+          </div>
+        </div>
+        <div className="output-content">
+          <p>{apiOutput}</p>
+        </div>
+      </div>
+    )}
 
       // Logo - Bottom Left Corner
       <div className="badge-container grow">
@@ -37,8 +93,9 @@ const Home = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
+        
+        <div className="badge">
+          <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>by chriscelaya</p>
           </div>
         </a>
